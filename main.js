b11653aafd4cd72c19e8911156da72f6af679d39
Alexander-Kitchenko <alexandr9001@gmail.com>
2022-05-27 22:46:36 +0300

    add_test_sasha

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 console.log('eh');
 
+const person = {
+   name: `Sasha`,
+   age: 35
+}
+
+const person2 = Object.assign({}, person)
+
+person2.age = 36
+
+console.log(person.age)
+console.log(person2.age)
+console.log(person)
+
+
