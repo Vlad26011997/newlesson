26ba076a0a0cfd0e2ebb0098f204c306473f0d20
Vlad26011997 <bilenko.vlad@ukr.net>
2022-05-31 17:33:19 +0300

    homework1

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('eh');
+const OPERATOR = prompt("Enter operator: +,-,/,*");
+const OPERANDA = Number(prompt("Enter operand A"));
+const OPERANDB = Number(prompt("Enter operand B"));
+let result;
+
+if (OPERATOR=="+") {
+    result = OPERANDA + OPERANDB;
+} else if (OPERATOR=="-") {
+result = OPERANDA - OPERANDB;
+} else if (OPERATOR=="/") {
+    result = OPERANDA / OPERANDB;
+} else if(OPERATOR=="*") {
+    result = OPERANDA * OPERANDB;
+}
+
+
